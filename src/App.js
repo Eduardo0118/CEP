a0eca34e52e15f0964bec915b199a53cd86e3c59
Eduardo Carneiro <eduardo.carneiro@bemobi.com>
2023-11-17 17:36:04 -0300

    Test deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { RecoilRoot } from "recoil";
+
+import Table from "./Components/Table/Table";
+import Form from "./Components/Form/Form";
+
+import "./Global/Style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RecoilRoot>
+      <Form />
+      <Table />
+    </RecoilRoot>
   );
 }
 
